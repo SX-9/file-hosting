774d6cfb2782175a2d8ff1baf9d301e900f804c8
DIBSTER <76603072+DEV-DIBSTER@users.noreply.github.com>
2023-02-27 21:37:16 -0500

    fix!: fixing errors/simplifying the code a bit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-#! /bin/node
-"use strict";
-
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const app = express();
-const { show_port, pass, PORT } = fs.existsSync('./config.json') ? JSON.stringify(fs.readFileSync('./config.json', 'UTF-8')) : process.env;
+const { show_port, pass, port } = fs.existsSync('./config.json') ? require('./config.json') : process.env;
 
-console.log('Starting With Configs:', show_port, pass, PORT)
+console.log('Starting With Configs:', show_port, pass, port)
 
 if (!fs.existsSync('./files')){
   fs.mkdirSync('./files');
@@ -40,8 +37,8 @@ app.post('/upload', (req, res) => {
     if (err) {
       return res.status(500).send(err);
     }
-    res.send('File Uploaded To ' + req.hostname + ( show_port ? ':' + PORT : '' ) + '/files/' + ( fs.readdirSync('./files').length - 1 ) + '_' + file.name);
+    res.send('File Uploaded To ' + req.hostname + ( show_port ? ':' + port : '' ) + '/files/' + ( fs.readdirSync('./files').length - 1 ) + '_' + file.name);
   });
 });
 
-app.listen(PORT, () => console.log('Online At ' + PORT));
+app.listen(port, () => console.log('Online At ' + port));
